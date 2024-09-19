947ba635d28acbfbf4b1d70d2ef5172198fd72e5
_null <100798042+null-Character7@users.noreply.github.com>
2024-09-19 13:58:33 +0530

    Landing page UI improved

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -4,11 +4,14 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Image from 'next/image';
-import hero from "../images/hero.jpg"
-import { FaGoogle } from 'react-icons/fa';
-import { useRouter } from 'next/navigation';
-
+import Image from "next/image";
+import hero from "../images/hero.jpg";
+import { FaGoogle } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+import { WobbleCard } from "./ui/wobble-card";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import { AuroraBackground } from "./ui/aurora-background";
+import { motion } from "framer-motion";
 
 export function Landing() {
   const { data: session } = useSession();
@@ -19,10 +22,10 @@ export function Landing() {
     if (userId) {
       // Redirect to /streams/userId
       console.log(userId);
-      
+
       router.push(`/streams`);
     } else {
-      console.error('User is not logged in');
+      console.error("User is not logged in");
     }
   };
 
@@ -92,145 +95,179 @@ export function Landing() {
                   </p>
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
-  {!session?.user && (
-    <Button
-      onClick={() => signIn("google")}
-      className="inline-flex h-10 items-center justify-center rounded-md px-8 text-sm font-medium shadow transition-colors hover:bg-[#FF6B6B]/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-    >
-      <FaGoogle className="h-4 w-4 mr-2" />
-      Sign Up with Google
-    </Button>
-  )}
+                  {!session?.user && (
+                    <Button
+                      onClick={() => signIn("google")}
+                      className="inline-flex h-10 items-center justify-center rounded-md px-8 text-sm font-medium shadow transition-all duration-300 transform hover:-translate-y-1 hover:shadow-lg hover:bg-black hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-100"
+                      >
+                      <FaGoogle className="h-4 w-4 mr-2" />
+                      Sign Up with Google
+                    </Button>
+                  )}
 
-  {session?.user && (
-    <Button
-    onClick={handleRedirect} // Assuming you have a route for "stream"
-      className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-muted px-8 text-sm font-medium shadow-sm text-black transition-colors hover:bg-muted-foreground/10 hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-    >
-      Go to My Space
-    </Button>
-  )}
+                  {session?.user && (
+                    <Button
+                      onClick={handleRedirect} // Assuming you have a route for "stream"
+                      className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-muted px-8 text-sm font-medium shadow-sm text-black transition-all duration-300 transform hover:-translate-y-1 hover:shadow-lg hover:bg-muted-foreground/20 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                      >
+                      Go to My Space
+                    </Button>
+                  )}
 
-  <Button
-    variant="outline"
-    className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-muted px-8 text-sm font-medium shadow-sm text-black transition-colors hover:bg-muted-foreground/10 hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-  >
-    Learn More
-  </Button>
-</div>
+                  <Button
+                    variant="outline"
+                    className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-muted px-8 text-sm font-medium shadow-sm text-black transition-colors hover:bg-muted-foreground/10 hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                  >
+                    Learn More
+                  </Button>
+                </div>
               </div>
               <div className="flex justify-center">
-                <Image
-                  src={hero} // Use the imported image here
-                  width="650"
-                  height="650"
-                  alt="Hero"
-                  className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-[#FFCDD2]">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-[#FFA500] px-3 py-1 text-sm text-[#FF6B6B]">
-                  Create Your Own Spaces
+                <div className="flex justify-center">
+                  <Image
+                    src={hero} // Use the imported image here
+                    width="650"
+                    height="650"
+                    alt="Hero"
+                    className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
+                  />
                 </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Curate Your Musical Oasis
-                </h2>
-                <p className="max-w-[900px] text-[#757575] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Build your own personal music spaces, where you and your
-                  friends can discover, share, and upvote songs. Customize the
-                  look and feel to match your unique vibe.
-                </p>
-              </div>
-            </div>
-            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-              {/* <img
-                src="/placeholder.svg"
-                width="550"
-                height="310"
-                alt="Image"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-              /> */}
-              <div className="flex flex-col justify-center space-y-4">
-                <ul className="grid gap-6">
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">
-                        Personalize Your Space
-                      </h3>
-                      <p className="text-[#757575]">
-                        Customize the look and feel of your music space to match
-                        your personal style.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">
-                        Collaborative Playlists
-                      </h3>
-                      <p className="text-[#757575]">
-                        Invite your friends to add songs and upvote their
-                        favorites, creating a dynamic and shared music
-                        experience.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">
-                        Seamless Integration
-                      </h3>
-                      <p className="text-[#757575]">
-                        Easily import songs from YouTube, Spotify, and other
-                        platforms to build your perfect playlist.
-                      </p>
-                    </div>
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
-          <div className="container grid items-center justify-center gap-4 px-4 md:px-6">
-            <div className="space-y-3">
-              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-                Discover New Music, Together
-              </h2>
-              <p className="mx-auto max-w-[600px] text-[#757575] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Explore music spaces created by your friends and the Soundscape
-                community. Upvote your favorite songs and contribute to the
-                collective vibe.
-              </p>
-            </div>
-            <div className="mx-auto w-full max-w-sm space-y-2">
-              <form className="flex gap-2">
-                <Input
-                  type="email"
-                  placeholder="Enter your email"
-                  className="max-w-lg flex-1"
-                />
-                <Button type="submit">Discover Premium</Button>
-              </form>
-              <p className="text-xs text-[#757575]">
-                Sign up to be notified {" "}
-                <Link
-                  href="#"
-                  className="underline underline-offset-2"
-                  prefetch={false}
-                >
-                  Terms &amp; Conditions
-                </Link>
-              </p>
+        <section className="w-full py-4 md:py-6 lg:py-12 bg-[#FFCDD2]">
+          <div className="container px-4 md:px-6">
+            <div className="mx-auto grid max-w-5xl items-center gap-6 py-0 lg:grid-cols-2 lg:gap-12">
+              <CardContainer className="inter-var">
+                <CardBody className="bg-zinc-900 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-10 border">
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-emerald-500 dark:via-blue-500 dark:to-purple-500"
+                  >
+                    Curate Your Musical Oasis
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-xl font text-indigo-600 dark:text-pink-300 mt-4"
+                  >
+                    Build your own personal music spaces, where you and your
+                    friends can discover, share, and upvote songs. Customize the
+                    look and feel to match your unique vibe.
+                  </CardItem>
+                  <div className="flex justify-between items-center mt-20">
+                    <CardItem
+                      translateZ={20}
+                      as={Link}
+                      href="/mySpace"
+                      target="__blank"
+                      onClick={() => signIn("google")}
+                      className="px-4 py-2 rounded-xl text-xs font-normal text-blue-600 dark:text-emerald-400"
+                    >
+                      Try now →
+                    </CardItem>
+                    <CardItem
+                      translateZ={20}
+                      as="button"
+                      onClick={() => signIn("google")}
+                      className="px-4 py-2 rounded-xl bg-gradient-to-r from-black via-gray-800 to-black dark:bg-gradient-to-r dark:from-white dark:via-gray-200 dark:to-white dark:text-black text-white text-xs font-bold"
+                    >
+                      Sign up
+                    </CardItem>
+                  </div>
+                </CardBody>
+              </CardContainer>
+              <CardContainer className="inter-var">
+                <CardBody className="bg-zinc-900 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-10 border">
+                  <CardItem
+                    as="h2"
+                    translateZ="50"
+                    className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-emerald-500 dark:via-blue-500 dark:to-purple-500"
+                  >
+                    Discover New Music, Together
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-xl font text-pink-600 dark:text-indigo-300 mt-4"
+                  >
+                    Explore music spaces created by your friends and the
+                    Soundscape community. Upvote your favorite songs and
+                    contribute to the collective vibe.
+                  </CardItem>
+                  <div className="flex justify-between items-center mt-20">
+                    <CardItem
+                      translateZ={20}
+                      as={Link}
+                      href="/mySpace"
+                      target="__blank"
+                      onClick={() => signIn("google")}
+                      className="px-4 py-2 rounded-xl text-xs font-normal text-blue-600 dark:text-emerald-400"
+                    >
+                      Try now →
+                    </CardItem>
+                    <CardItem
+                      translateZ={20}
+                      as="button"
+                      onClick={() => signIn("google")}
+                      className="px-4 py-2 rounded-xl bg-gradient-to-r from-black via-gray-800 to-black dark:bg-gradient-to-r dark:from-white dark:via-gray-200 dark:to-white dark:text-black text-white text-xs font-bold"
+                    >
+                      Sign up
+                    </CardItem>
+                  </div>
+                </CardBody>
+              </CardContainer>
             </div>
           </div>
         </section>
+        <AuroraBackground>
+          <motion.div
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="relative flex flex-col gap-4 items-center justify-center px-4"
+          >
+            <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+              <div className="container grid items-center justify-center gap-4 px-4 md:px-6">
+                <div className="space-y-3">
+                  <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight text-white">
+                    Join Premium
+                  </h2>
+                  <p className="mx-auto max-w-[600px] text-[#757575] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                    Join premium to get early Beta access and other premium
+                    features
+                  </p>
+                </div>
+                <div className="mx-auto w-full max-w-sm space-y-2">
+                  <form className="flex gap-2">
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="max-w-lg flex-1 text-teal-50"
+                    />
+                    <Button type="submit">Discover Premium</Button>
+                  </form>
+                  <p className="text-xs text-[#757575]">
+                    Sign up to be notified{" "}
+                    <Link
+                      href="#"
+                      className="underline underline-offset-2"
+                      prefetch={false}
+                    >
+                      Terms &amp; Conditions
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </section>
+          </motion.div>
+        </AuroraBackground>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-[#757575]">
